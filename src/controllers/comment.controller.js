c3df01a2da863fce8ed4e047ed4a04186223c2ab
Meet <codewithmeet25@gmail.com>
2024-04-17 16:50:09 +0530

    comment controller done as of now

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -3,8 +3,9 @@ import { Comment } from "../models/comment.model.js"
 import { asyncHandler } from "../utils/AsyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
+import {Like} from "../models/likes.model.js"
 
-const addComment = asyncHandler(async(req, res)=>{
+const addBlogComment = asyncHandler(async(req, res)=>{
     const {blogId} = req.params
     const {content} = req.body
 
@@ -31,6 +32,81 @@ const addComment = asyncHandler(async(req, res)=>{
     )
 })
 
+const deleteBlogComment = asyncHandler(async(req, res)=>{
+    const {commentId} = req.params
+
+    if(!isValidObjectId(commentId)){
+        throw new ApiError(400, "Invalid blogId")
+    }
+
+    const comment = await Comment.findById(commentId)
+
+    if(!comment){
+        throw new ApiError(404, "comment does not found")
+    }
+
+    if(comment?.owner.toString() !== req.user?._id.toString()){
+        throw new ApiError(400, "only owner can delete the comment")
+    }
+
+    const deleteComment = await Comment.findByIdAndDelete(commentId)
+    if(!deleteComment){
+        throw new ApiError(500, "something went wrong while deleting comment")
+    }
+
+    await Like.deleteMany({
+        comment:commentId,
+        likedBy:req.user
+    })
+
+    return res.status(201).json(
+        new ApiResponse(201, {}, "delete comment successfully")
+    )
+
+})
+
+const updateBlogComment = asyncHandler(async(req, res)=>{
+    const {commentId} = req.params
+    const {content} = req.body
+
+    if(!isValidObjectId(commentId)){
+        throw new ApiError(400, "Invalid commentId")
+    }
+
+    const comment = await Comment.findById(commentId)
+
+    if(!comment){
+        throw new ApiError(404, "comment not found")
+    }
+
+    if(comment?.owner.toString() !== req.user?._id.toString()){
+        throw new ApiError(400, "only owner can update comment")
+    }
+
+    const updateComment = await Comment.findByIdAndUpdate(
+        commentId,
+        {
+            $set:{
+                content
+            }
+        },
+        {
+            new:true
+        }
+    )
+
+    if(!updateComment){
+        throw new ApiError(500, "something went wrong while update comment")
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, updateComment, "comment update successfully")
+    )
+})
+
+
 export {
-    addComment
+    addBlogComment,
+    deleteBlogComment,
+    updateBlogComment
 }
--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
-import { addComment } from "../controllers/comment.controller.js";
+import { addBlogComment, deleteBlogComment, updateBlogComment } from "../controllers/comment.controller.js";
 
 const router = Router()
 
 router.use(verifyJwt)
 
-router.route("/:blogId").post(addComment)
+router.route("/b/:blogId")
+.post(addBlogComment)
+router.route("/c/:commentId").patch(updateBlogComment).delete(deleteBlogComment)
+
 
 
 export default router
